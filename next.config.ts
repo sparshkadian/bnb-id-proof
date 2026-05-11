6dab0307d2ab1bd3c476edb5d1691613c608d8e0
sparsh-DBALounge <sparsh.kadian@dbalounge.com>
2026-05-11 09:09:09 +0530

    fixed experimental warnings

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client"],
-  },
+  serverExternalPackages: ["@prisma/client"],
   turbopack: {
     root: "C:/Users/spars/Desktop/BnB/idproof",
   },
